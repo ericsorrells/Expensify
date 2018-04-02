13552824b0f1df2dc7dda439cc16cd247ce657eb
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-02 15:51:27 -0400

    7.1: Add Switch for 404 Pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -20,14 +20,18 @@ const HelpPage = () => (
   <div>HELP PAGE</div>
 );
 
+const NotFoundPage = () => (
+  <div>404!!!</div>
+)
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpansePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 )
 
