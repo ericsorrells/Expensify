d002792b110a6892209555a637cceee3cc4e589b
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-02 17:41:38 -0400

    7.4: Query Strings And URL Params

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const EditExpense = () => (
-  <div>EDIT EXPENSE PAGE</div>
-);
-
+// const EditExpense = () => (
+//   <div>EDIT EXPENSE PAGE</div>
+// );
+const EditExpense = (props) => {
+  console.log('PROPS', props)
+    return (
+      <div>
+        <div>EDIT EXPENSE PAGE</div>
+        <br/>
+        <div>Editing Expense Number {props.match.params.id}</div>
+      </div>
+    )
+  };
 export default EditExpense;
