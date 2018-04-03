a9eb7f4905e7971507a6650aa6f1246483a5ab72
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-03 15:30:59 -0400

    8.5: Reducers Intro

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,29 +1,31 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
-      return {
-        count: state.count + incrementBy
-      } 
-    case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
-      return {
-        count: state.count - decrementBy
-      }  
-    case 'RESET':
-      return {
-        count: 0
-      }
-    case 'SET':
-      return {
-        count: action.count
-      }  
-    default: 
-      return state
-  }
-});
+const countReducer = (state = { count: 0 }, action) => {
+    switch(action.type) {
+      case 'INCREMENT':
+        const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+        return {
+          count: state.count + incrementBy
+        } 
+      case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+        return {
+          count: state.count - decrementBy
+        }  
+      case 'RESET':
+        return {
+          count: 0
+        }
+      case 'SET':
+        return {
+          count: action.count
+        }  
+      default: 
+        return state
+    }
+};
+
+const store = createStore((state = { count: 0 }, action) => );
 
 // incrementing
 const unsubscribe = store.subscribe(() => {
