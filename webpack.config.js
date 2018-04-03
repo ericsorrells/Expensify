6a4fe16c184394784dd3937f9568e83c6b93526c
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-03 08:59:33 -0400

    8.1: Redux Actions

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 // entry: define entry poinVt
 // output: define location of final bundle
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/playground/redux-101.js',
   output: {
     path: path.join(__dirname, 'public'),  // absolute path to where to output
     filename: 'bundle.js'                  //can use any name
