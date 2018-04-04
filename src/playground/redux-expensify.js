a230a683086a9d6bef7fe248af6c3632e0be45ba
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-03 20:12:59 -0400

    8.7: Spread Objects in Reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,70 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+const addExpense = ({description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount, 
+    createdAt
+  }
+})
+
+const removeExpense= ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+})
+
+
+
+const setTextFitler = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+}) 
 
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch(action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense 
+      ]
+    case 'REMOVE_EXPENSE':
+      return state.filter((expense) => expense.id !== action.id); 
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense;
+        };
+      });  
     default:
+    return state;
+  }
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch(action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      }
+    default: 
       return state;
   }
 }
@@ -16,22 +76,23 @@ const filtersReducerDefaultState = {
   endDate: undefined 
 };
 
-const filtersReducer = (state = filtersReducerDefaultState, action) => {
-  switch(action.type) {
-    default:
-      return state;
-  }
-};
-
-
 const store = createStore(
   combineReducers({
     expenses: expensesReducer,
     filters: filtersReducer
   })
 );
-console.log('STORE', store.getState());
 
+store.subscribe(() => {
+  console.log('STORE_SUBSCR:', store.getState())
+})
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 400}));
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 10000 }))
+
+store.dispatch(setTextFitler( 'My New Text' ));
 const demoState = {
   expenses: [{
     id: 1,
@@ -47,3 +108,5 @@ const demoState = {
     endDate: undefined
   }
 }
+
+
