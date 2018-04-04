77b040e7719933045f3737439fdcc036db0f10b4
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-04 08:36:13 -0400

    9.0: Organizing React & Redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import * as filters from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water Bill'}));
+store.dispatch(addExpense({description: 'Gas Bill'}));
+store.dispatch(filters.setTextFitler('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log('VIS', visibleExpenses);
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
