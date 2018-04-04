b02f3405474d8b18a3ef57b9bc3681b8ed231871
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-04 16:56:42 -0400

    9.3: Rendering Indiv Expenses

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill'}));
+store.dispatch(addExpense({description: 'Water Bill', amount: 4500}));
 store.dispatch(addExpense({description: 'Gas Bill'}));
-store.dispatch(filters.setTextFitler('water'));
+store.dispatch(filters.setTextFitler('bill'));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses'
 
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List!!!</h1>
-    <p>{props.expenses.length}</p>
+    <div>{props.expenses.map((expense, index) => <ExpenseListItem {...expense} key={expense.id} />)}</div>
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
-    expenses: state.expenses
+    expenses: selectExpenses(state.expenses, state.filters) 
   }
 } 
 
