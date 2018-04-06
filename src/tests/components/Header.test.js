3aab1a1c9d699e0b8575c2a6ca7e81971be1f370
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-06 12:06:13 -0400

    10.6: Setting Up Enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('HEADER: should render correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    console.log(renderer.getRenderOutput());
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const component = shallow(<Header />);
+  expect(component).toMatchSnapshot();
+});
+
+test('should have correct header text', () => {
+  const component = shallow(<Header />);
+  expect(component.find('h1').text()).toBe('Expensify'); 
 })
 
