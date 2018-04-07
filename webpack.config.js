cb56cb813d4b6858a666b1dc23bfd935185a3156
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-07 12:08:15 -0400

    11.2: Prod Webpack Build w Separate CSS Files and SourceMaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,49 @@
 //add Node path module
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-// node method for exporting
-// entry: define entry poinVt
-// output: define location of final bundle
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),  // absolute path to where to output
-    filename: 'bundle.js'                  //can use any name
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,                      // $ = all files
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader"
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    // can provide any option in at https://webpack.js.org/configuration/dev-server/
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-}
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css'); // name of file you choose
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),  // absolute path to where to output
+      filename: 'bundle.js'                  //can use any name
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,                      // $ = all files
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({           // tells webpack to extrack any of these files
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      // can provide any option in at https://webpack.js.org/configuration/dev-server/
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
+};
