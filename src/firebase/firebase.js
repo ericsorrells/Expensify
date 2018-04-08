aca44cec6d2fe07e9102d6349c55b8f637de5f23
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-08 09:39:52 -0400

    12.0: set up Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,28 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAkig9wGtw0KTYxwVR5hJ5W3bXtjbCChU8",
+  authDomain: "expensify-40db4.firebaseapp.com",
+  databaseURL: "https://expensify-40db4.firebaseio.com",
+  projectId: "expensify-40db4",
+  storageBucket: "expensify-40db4.appspot.com",
+  messagingSenderId: "350243492178"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+database.ref().set({
+  name: 'Eric Sorrells',
+  age: 26, 
+  isSingle: false,
+  location: {
+    city: 'Atlanta',
+    country: 'USA'
+  }
+});
+
+database.ref('age').set(34);
+database.ref('location/city').set('Rome');
+database.ref('attributes').set({ height: 61, weight: 150 })
