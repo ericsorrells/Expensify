9861ef719cf8d0aab4e9d2f474102bcbe6bcfa5f
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-09 17:35:55 -0400

    12.7: ENV Variables & Separate Test DB

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyAkig9wGtw0KTYxwVR5hJ5W3bXtjbCChU8",
-  authDomain: "expensify-40db4.firebaseapp.com",
-  databaseURL: "https://expensify-40db4.firebaseio.com",
-  projectId: "expensify-40db4",
-  storageBucket: "expensify-40db4.appspot.com",
-  messagingSenderId: "350243492178"
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
+
 firebase.initializeApp(config);
 
 const database = firebase.database();
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,41 @@
-//add Node path module
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
+}
 
 module.exports = (env) => {
   const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css'); // name of file you choose
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public', 'dist'),  // absolute path to where to output
-      filename: 'bundle.js'                  //can use any name
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
     },
     module: {
       rules: [{
         loader: 'babel-loader',
-        test: /\.js$/,                      // $ = all files
+        test: /\.js$/,
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: CSSExtract.extract({           // tells webpack to extrack any of these files
+        use: CSSExtract.extract({
           use: [
             {
               loader: 'css-loader',
               options: {
                 sourceMap: true
               }
-            }, {
+            },
+            {
               loader: 'sass-loader',
               options: {
                 sourceMap: true
@@ -37,11 +46,18 @@ module.exports = (env) => {
       }]
     },
     plugins: [
-      CSSExtract
+      CSSExtract,
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+      })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-      // can provide any option in at https://webpack.js.org/configuration/dev-server/
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
       publicPath: '/dist/'
