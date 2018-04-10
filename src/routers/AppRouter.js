603e5586a0c9f6addf20d32f8284e005716f421b
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-10 17:44:05 -0400

    12.14: Validation & Private Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,22 +4,21 @@ import createHistory from 'history/createBrowserHistory';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import ExpenseDashboard from '../components/ExpenseDashboard';
-import Header from '../components/Header';
 import Help from '../components/Help';
 import NotFound from '../components/NotFound';
 import LogIn from '../components/LogIn';
+import PrivateRoute from './PrivateRoute';
 
 const history = createHistory();
 
 const AppRouter = () => (
   <Router history={ history }>
     <div>
-      <Header/>
       <Switch>
         <Route path="/" component={LogIn} exact />
-        <Route path="/dashboard" component={ExpenseDashboard} />
-        <Route path="/create" component={AddExpense} />
-        <Route path="/edit/:id" component={EditExpense} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboard} />
+        <PrivateRoute path="/create" component={AddExpense} />
+        <PrivateRoute path="/edit/:id" component={EditExpense} />
         <Route path="/help" component={Help} />
         <Route component={NotFound} />
       </Switch>
